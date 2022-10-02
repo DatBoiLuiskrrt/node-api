3abfa2b218768b5e513953c38b025c2e17a4b1ce
datboiluiskrrt <datboiluiskrrt@gmail.com>
2022-10-02 00:53:56 -0500

    Created api/hubs endpoint

--- README.md
+++ README.md
@@ -3,3 +3,7 @@
 -npm init -y //creates package.json
 -npm i express //starts express
 -npx gitignore node //generate gitgnore ready for node
+-node index.js
+-npm i -D nodemon
+-configured "server" script to use: "nodemon index.js"
+-npm run server
--- index.js
+++ index.js
@@ -7,6 +7,24 @@ server.get("/", (req, res) => {
   res.send("<h1>A mi el mencho me pela la verga</h1>");
 });
 
+server.get("/api/hubs", (req, res) => {
+  let lessons = [
+    {
+      id: 1,
+      name: "Web 40 node intro",
+      lessonId: 1,
+      cohort: "web 30",
+    },
+    {
+      id: 2,
+      name: "Web 50 node intro",
+      lessonId: 2,
+      cohort: "web 50",
+    },
+  ];
+  res.json(lessons);
+});
+
 const PORT = 8000; // we visit http://localhost:8000/ to see the api
 server.listen(PORT, () =>
   console.log(`Hello minetito hermosho, server is running ${PORT}`)
